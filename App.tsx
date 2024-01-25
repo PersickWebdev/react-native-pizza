4fd93b7a98e767f326e9cdc44353dd0000847417
Vlad <vladdev461@gmail.com>
2024-01-25 22:40:31 +0200

    feature (ProductListScreen): ProductListScreen component created

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ProductListScreen } from '@/screens/ProductListScreen/ProductListScreen';
 
 export default function App() {
-  return (
-      <SafeAreaProvider>
-        <StatusBar style="light" />
-      </SafeAreaProvider>
-  );
-}
+    return (
+        <SafeAreaProvider>
+            <StatusBar style="light" />
+            <ProductListScreen />
+        </SafeAreaProvider>
+    );
+};
---
+++ src/screens/ProductListScreen/ProductListScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
+
+const ProductListScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>
+                ProductListScreen
+            </Text>
+        </SafeAreaView>
+    );
+};
+
+export { ProductListScreen };
